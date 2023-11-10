f7c1be9a888d7eeb090e8a547cf14433a46a3e85
profissional09 <127982510+EZEQUIELFILHO383@users.noreply.github.com>
2023-11-09 22:54:30 -0300

    https://github.com/EZEQUIELFILHO383/projeto-site.git

---
+++ script.js
@@ -0,0 +1,12 @@
+let form = document.querySelector(".fale-conosco")
+let mask = document.querySelector(".mascara-form")
+
+function project(){
+    form.style.left = ("700px")
+    mask.style.visibility = ("visible")
+}
+
+function toBeGone(){
+    form.style.left = ("-320px")
+    mask.style.visibility = ("hidden")
+}
